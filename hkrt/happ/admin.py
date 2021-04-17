77c7a343e80087e091a8c146349bd4e7c833d2be
dmunige <82717168+dmunige@users.noreply.github.com>
2021-04-17 20:41:26 +0530

    Add files via upload

---
+++ hkrt/happ/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from.models import customer,master
+admin.site.register(customer)
+admin.site.register(master)
+# Register your models here.
---
+++ hkrt/happ/forms.py
@@ -0,0 +1,5 @@
+# from django.forms import Form
+
+# class homeform(forms.Form):
+#     School=forms.CharField( max_length=100, required=False)
+#     Village=forms.CharField( max_length=100, required=False)
